70b89713c55420c76b28cbc9a8fc4ec13efde30a
Philip Van Raalte <phil.vanraalte@gmail.com>
2020-01-12 18:33:11 -0500

    added type checking for routeBinder decorators

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,10 +1,15 @@
 import "reflect-metadata";
 import { Method } from "./Method";
 import { MetadataKey } from "./MetadataKey";
+import { RequestHandler } from "express";
+
+interface RouteHandlerDesriptor extends PropertyDescriptor {
+  value?: RequestHandler;
+}
 
 function routeBinder(method: Method) {
   return function(path: string) {
-    return function(target: any, key: string, desc: PropertyDescriptor) {
+    return function(target: any, key: string, desc: RouteHandlerDesriptor) {
       Reflect.defineMetadata(MetadataKey.Path, path, target, key);
       Reflect.defineMetadata(MetadataKey.Method, method, target, key);
     };
